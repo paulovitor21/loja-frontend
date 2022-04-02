034b2aeab056300e0fc4a31fa7a7b55d31f14b9a
Paulo Vitor <80797702+paulovitor21@users.noreply.github.com>
2022-04-02 19:46:01 -0400

    prototipos do frontend para o sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CarrosselComponent } from './componentes/carrossel/carrossel.component';
+import { DestaquesComponent } from './componentes/destaques/destaques.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RodapeComponent,
+    CarrosselComponent,
+    DestaquesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/destaques/destaques.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/model/Produto';
+import { ProdutoService } from 'src/app/servicos/produto.service';
+
+@Component({
+  selector: 'app-destaques',
+  templateUrl: './destaques.component.html',
+  styleUrls: ['./destaques.component.css']
+})
+export class DestaquesComponent implements OnInit {
+  public lista!: Produto[];
+
+
+  // preciso injetar o servico que busca o produto
+  constructor(private service: ProdutoService) {
+
+  }
+
+  ngOnInit(): void {
+     this.service.getAllProdutos()
+        .subscribe((res: Produto[]) => {
+                      this.lista = res;
+             },
+             err => console.log(err));
+  }
+
+}
